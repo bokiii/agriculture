ce0486f3e15d3f14698129e5c62bcf3fcd224d68
nephets <nephets.dureza@gmail.com>
2016-09-27 22:10:45 +0800

    updated the admin buttons

--- application/views/admin_view.php
+++ application/views/admin_view.php
@@ -19,137 +19,23 @@
 		<div class="container">
 			<div class="row intro-tables">
 				
-				<div class="col-md-6">
-					<div class="intro-table intro-table-first">
-						<h5 class="white heading">Schedule of Activities</h5>
-						<div class="owl-carousel owl-schedule bottom">
-							<div class="item">
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Early Exercise</h5>
-									</div>
-										<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Muscle Building</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Cardio</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Early Exercise</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Muscle Building</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Cardio</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Early Exercise</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Muscle Building</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-								<div class="schedule-row row">
-									<div class="col-xs-6">
-										<h5 class="regular white">Cardio</h5>
-									</div>
-									<div class="col-xs-6 text-right">
-										<h5 class="white">8:30 - 10:00</h5>
-									</div>
-								</div>
-							</div>
-						</div>
+				<div class="col-md-3">
+					<div class="intro-table">  
+						<button type="button" class="btn btn-primary">Users</button>
 					</div>
-				</div> <!-- end col md 4 -->
+				</div> <!-- end col md 3 -->
 				
-				<div class="col-md-6">
-					<div class="intro-table intro-table-third">
-						<h5 class="white heading">Farmers Testimonials</h5>
-						<div class="owl-testimonials bottom">
-							<div class="item">
-								<h4 class="white heading content">I couldn't be more happy with the results!</h4>
-								<h5 class="white heading light author">Adam Jordan</h5>
-							</div>
-							<div class="item">
-								<h4 class="white heading content">I can't believe how much better I feel!</h4>
-								<h5 class="white heading light author">Greg Pardon</h5>
-							</div>
-							<div class="item">
-								<h4 class="white heading content">Incredible transformation and I feel so healthy!</h4>
-								<h5 class="white heading light author">Christina Goldman</h5>
-							</div>
-						</div>
+				<div class="col-md-3">
+					<div class="intro-table">
+						<button type="button" class="btn btn-primary">Categories</button>
 					</div>
-				</div> <!-- end col md 4 -->  
+				</div> <!-- end col md 3 --> 	 
 
 			</div>
 		</div>
 	</section>  
 
-	<!-- Below section for crud -->   
-	<section>  
-		<div class="container">  
-			<div class="col-md-12">  
-				<div class="table-responsive">
-					<table class="table">
-						<thead>
-							<th>Head</th>
-							<th>Head</th>
-							<th>Head</th>  
-						</thead>  
-						<tbody>  
-							<td>Value</td>
-							<td>Value</td>
-							<td>Value</td>	
-						</tbody>
-					</table>
-				</div>	
-			</div>
-		</div>
-	</section>
+	
 	
 	
 
